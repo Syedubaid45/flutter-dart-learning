173c6bbd6fda85f37e2c4d9566d27398fa477779
Syedubaid45 <syedubaid4567@gmail.com>
2025-04-26 19:58:06 +0500

    Added Assignment 1 - Variables and Introduction program

---
+++ Assignment_1_Variables_Introduction/assignment_1.dart
@@ -0,0 +1,16 @@
+void main() {
+  String name = "Ubaid Ur Rehman Shah";
+  int age = 20;
+  String course = "Flutter App Development";
+  double percentage = 77.5;
+  String previousResult = "First Division";
+
+  print("Introduction:");
+
+  print(
+    "My name is $name, a $age year old student of $course. "
+    "I have a strong academic record with a $percentage% score "
+    "and my previous result was $previousResult. "
+    "I am passionate about creating innovative mobile applications.",
+  );
+}
